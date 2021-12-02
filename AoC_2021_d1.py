93c8cf3ad7d95de80d07f0b3432e7c1559c377ec
imnotartsy <therese.patrosio@gmail.com>
2021-12-02 00:19:32 -0500

    adds first two days of AoC 2021

---
+++ AoC_2021_d1.py
@@ -0,0 +1,57 @@
+# https://www.codegrepper.com/code-examples/python/how+to+read+a+file+into+array+in+python
+def readFile(fileName):
+        fileObj = open(fileName, "r") #opens the file in read mode
+        words = fileObj.read().splitlines() # puts the file into an array
+        fileObj.close()
+        return words
+
+def increaseCount(arr):
+    count = 0
+    for i in range(1, len(arr)):
+        if arr[i] > arr[i-1]:
+            count += 1
+    return(count)
+
+# def slidingWindow(arr):
+#     count = 0
+#     for i in range(3, len(arr)):
+#         print(arr[i-1], arr[i-2], arr[i-3], "\t", int(arr[i-1]) + int(arr[i-2]) + int(arr[i-3]), "\t compared to\t", arr[i], arr[i-1], arr[i-2], "\t", int(arr[i]) + int(arr[i-1]) + int(arr[i-2]))
+        
+#         if int(arr[i]) + int(arr[i-1]) + int(arr[i-2]) > int(arr[i-1]) + int(arr[i-2]) + int(arr[i-3]):
+#             count += 1
+#             print("increase!")
+#     return(count)
+
+def slidingWindow(arr):
+    count = 0
+
+    leftSum = int(arr[0]) + int(arr[1]) + int(arr[2])
+    rightSum = int(arr[1]) + int(arr[2]) + int(arr[3])
+
+    for i in range(0, len(arr)-4):    
+        if rightSum > leftSum:
+            count += 1
+
+        leftSum = leftSum - int(arr[i]) + int(arr[i+3])
+        rightSum = rightSum - int(arr[i+1]) + int(arr[i+4])
+
+
+    return(count)
+
+# arr = [
+#     199,
+#     200,
+#     208,
+#     210,
+#     200,
+#     207,
+#     240,
+#     269,
+#     260,
+#     263
+# ]
+
+arr = readFile("AoC_2021_d1_input2.txt")
+
+print(slidingWindow(arr))
+print(increaseCount(arr))
---
+++ AoC_2021_d2.py
@@ -0,0 +1,55 @@
+# https://www.codegrepper.com/code-examples/python/how+to+read+a+file+into+array+in+python
+from os import read
+
+def readFile(fileName):
+        fileObj = open(fileName, "r") #opens the file in read mode
+        words = fileObj.read().splitlines() # puts the file into an array
+        fileObj.close()
+        return words
+
+def distance(arr):
+    x = 0
+    y = 0
+    for line in arr:
+        splitin = line.split()
+        dir = splitin[0]
+        dist = int(splitin[1])
+
+        if dir == "forward":
+            x += dist
+        elif dir == "up":
+            y += dist
+        elif dir == "down":
+            y -= dist
+    
+    print("X:", x, "Y:", y)
+
+    return abs(x * y)
+
+def distance2(arr):
+    x = 0
+    y = 0
+    aim = 0
+    for line in arr:
+        splitin = line.split()
+        dir = splitin[0]
+        dist = int(splitin[1])
+
+        if dir == "forward":
+            # depth by your aim multiplied by X.
+            y += aim * dist
+            x += dist   
+        elif dir == "up":
+            aim -= dist
+        elif dir == "down":
+            aim += dist
+    
+    print("X:", x, "Y:", y)
+
+    return x * y
+
+arr = readFile("AoC_2021_d2_input.txt")
+print(distance(arr))
+print(distance2(arr))
+
+# 00:06:50; first tries
